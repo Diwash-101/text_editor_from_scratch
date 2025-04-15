00149d46c8131557ffffe478826ed41e8fc88b07
Diwash-101 <76509157+Diwash-101@users.noreply.github.com>
2025-04-15 16:44:28 -0400

    basic I/O

---
+++ TextEditor.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+class TextEditor {
+
+       public static void main(String[] args)
+    {
+
+        // Creating Scanner class object
+        Scanner scn = new Scanner(System.in);
+
+
+        String b = scn.nextLine();
+
+        // File file = new File("untitled.txt");
+
+        System.out.print(b);
+
+        // Closing the scanner to release resources
+        scn.close();
+    }}
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Commit everyday challenge
